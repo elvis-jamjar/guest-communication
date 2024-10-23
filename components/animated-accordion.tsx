31c5b3ef17c9abc4bbfee00f4cc6ba0679514e4b
Elvis Kemevor <elvis@jamjargh.com>
2024-10-23 15:28:53 +0000

    updated

---
+++ components/animated-accordion.tsx
@@ -0,0 +1,79 @@
+'use client';
+import { cn } from '@/lib/utils';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ChevronDown } from 'lucide-react';
+import React, { useState } from 'react';
+
+interface AccordionItem {
+    title: string | React.ReactNode,
+    className?: string,
+    children: React.ReactNode | string
+}
+
+interface AnimatedAccordionProps {
+    items: AccordionItem[]
+}
+
+export function ReusableAnimatedAccordion({ items }: AnimatedAccordionProps) {
+    const [expandedIndex, setExpandedIndex] = useState<number | null>(null)
+    const [isMobile, setIsMobile] = React.useState(false);
+    React.useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setIsMobile(window.innerWidth < 768);
+        }
+        // onpage resize listen to window size
+        window.addEventListener('resize', () => {
+            setIsMobile(window.innerWidth < 768);
+        });
+    }, []);
+    const toggleItem = (index: number) => {
+        setExpandedIndex(expandedIndex === index ? null : index)
+    }
+
+    return (
+        <div className="w-full space-y-2">
+            {items.map((item, index, arr) => (
+                <div key={index} className={cn("border-2 border-x-[0.0px] border-t-0 border-primary-main rounded-b-3xl overflow-hidden", ((arr?.length - 1) == index) && 'border-0 border-transparent')}>
+                    <motion.button
+                        className="flex justify-between items-center w-full px-4 py-4 text-left bg-white hover:bg-gray-50 focus:outline-none"
+                        onClick={() => toggleItem(index)}
+                        initial={false}
+                        aria-expanded={expandedIndex === index}
+                        aria-controls={`accordion-content-${index}`}
+                    >
+                        {/* <span className={cn("font-medium text-gray-900", item?.className)}></span> */}
+                        {item.title}
+                        <motion.span
+                            animate={{ rotate: expandedIndex === index ? 180 : 0 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <ChevronDown className="w-5 h-5 text-gray-500" aria-hidden="true" />
+                        </motion.span>
+                    </motion.button>
+                    <AnimatePresence initial={false}>
+                        {expandedIndex === index && (
+                            <motion.div
+                                id={`accordion-content-${index}`}
+                                initial="collapsed"
+                                animate="expanded"
+                                exit="collapsed"
+                                variants={{
+                                    expanded: { opacity: 1, maxHeight: isMobile ? "100%" : "200dvh" },
+                                    collapsed: { opacity: 0, maxHeight: "0px" }
+                                }}
+                                transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                                className="overflow-auto"
+                            >
+                                {/* <div className="px-4 py-3 text-gray-700 bg-gray-50 h-[200px] overflow-y-auto">
+                                    x
+                                    </div> */}
+                                <div className="px-4 py-3">{item.children}</div>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </div>
+            ))}
+        </div>
+    )
+}
+
--- components/schedule-list.tsx
+++ components/schedule-list.tsx
@@ -1,6 +1,7 @@
 'use client';;
 import { ConferenceScheduleProps } from "@/app/types";
 import { cn } from "@/lib/utils";
+import { ReusableAnimatedAccordion } from "./animated-accordion";
 import { ConferenceSchedule } from "./conference-schedule";
 // import { useParams } from "next/navigation";
 // import { useParams } from "next/navigation";
@@ -9,6 +10,7 @@ import { ConferenceSchedule } from "./conference-schedule";
 export function ScheduleList(
     { schedules }: { schedules: ConferenceScheduleProps[], columns?: number }
 ) {
+
     // const { userId } = useParams<{ userId?: string }>();
     // const isSingle = userId ? 'single' : 'multiple';
     // const defaultValue = userId ? [`item-0`] : ["item-0"];
@@ -33,38 +35,50 @@ export function ScheduleList(
         //     //     ))}
         //     // </Accordion>
 
-        <div className="ring-primary-main ring-2 rounded-[30px] mx-2 bg-white">
-            <div className="bg-primary-main p-2 flex justify-center rounded-t-[30px]">
+        <div className="border-primary-main border-2 rounded-[30px] mx-2 bg-white">
+            <div className="bg-primary-main p-2 flex justify-center rounded-t-[28px]">
                 <h3
                     style={{
                         fontSize: "clamp(1rem, 2vw, 2rem)"
                     }}
                     className="text-white font-semibold">3rd - 6th November</h3>
             </div>
-            <div className="md:p-16 p-4 space-y-5">
-                {
-                    schedules.map((schedule, index) => {
-                        return (
-                            <div key={"main-" + index} className="flex flex-wrap md:flex-nowrap relative gap-4 ">
-                                <div className="md:max-w-28 w-full">
-                                    <h2 className={cn("text-2xl font-bold text-primary-main", schedule?.color?.day)}>
+
+            <div className="md:py-4 md:pb-1 space-y-8">
+                <ReusableAnimatedAccordion
+                    items={
+                        schedules?.map((schedule, index) => {
+                            return {
+                                title: <div className="flex w-full items-center gap-2">
+                                    <h2 className={cn("md:text-2xl text-lg mr-2 font-bold text-primary-main", schedule?.color?.day)}>
                                         {schedule.day}
                                     </h2>
-                                    <p className={cn("text-orange-500 font-semibold", schedule?.color?.dayTitle)}>
+                                    <p className={cn("text-secondary-main md:text-lg text-sm font-semibold", schedule?.color?.dayTitle)}>
                                         {schedule?.title}
                                     </p>
+                                </div>,
+                                children: <div key={"main-" + index} className="flex flex-wrap md:flex-nowrap relative gap-4 ">
+                                    <div className="md:max-w-28 w-full">
+                                        <h2 className={cn("text-2xl font-bold text-primary-main", schedule?.color?.day)}>
+                                            {schedule.day}
+                                        </h2>
+                                        <p className={cn("text-secondary-main font-semibold", schedule?.color?.dayTitle)}>
+                                            {schedule?.title}
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="flex-grow text-lg font-thin leading-snug tracking-tight text-balance">
+                                        <ConferenceSchedule
+                                            {...schedule}
+                                        />
+                                    </div>
                                 </div>
-                                <div
-                                    // leading-5 
-                                    className="flex-grow text-lg font-thin leading-snug tracking-tight text-balance">
-                                    <ConferenceSchedule
-                                        {...schedule}
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                            }
+                        }) || []
+                    }
+
+                />
+
 
             </div>
 
