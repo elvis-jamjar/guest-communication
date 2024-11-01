3191feaebde835e6ea7dd8b930737703cecf386b
Elvis Kemevor <elvis@jamjargh.com>
2024-11-01 19:49:45 +0000

    Update page.tsx

--- app/upload-cropper/page.tsx
+++ app/upload-cropper/page.tsx
@@ -5,9 +5,11 @@ export default function Page() {
 
     return (
         <div className="w-full h-screen">
-            <ImageCropper onCompleteUpload={(url) => {
-                // do something with the url
-            }} />
+            <ImageCropper
+                btnTitle="Upload Image"
+                onCompleteUpload={(url) => {
+                    // do something with the url
+                }} />
         </div>
 
     )
