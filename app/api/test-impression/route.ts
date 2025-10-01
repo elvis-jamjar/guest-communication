4e880bde767446efb0d8bd332a175f1ffddc630e
Elvis Kemevor <elvis@jamjargh.com>
2025-10-01 22:20:29 +0000

    Update route.ts

--- app/api/test-impression/route.ts
+++ app/api/test-impression/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getNotifications } from "@/app/actions/timeline";
 
-export async function GET(req: NextRequest) {
+export async function GET(_req: NextRequest) {
   try {
     const notifications = await getNotifications();
 
