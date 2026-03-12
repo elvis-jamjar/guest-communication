91df02cddf2c67016fd9b342d5877a2cb5e2e41f
Elvis Kemevor <elvis@jamjargh.com>
2026-03-12 16:37:47 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,35 +8,35 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  title: "4DX CEO Summit 2024",
+  title: `4DX CEO Summit ${new Date().getFullYear()}`,
   description: "",
   // openGraph
   openGraph: {
-    title: "4DX CEO Summit 2024",
+    title: `4DX CEO Summit ${new Date().getFullYear()}`,
     description: "Join us for insightful discussions, networking opportunities, and strategic collaborations shaping the future of technology and innovation across Africa",
     url: "https://4dxceosummit.com",
     images: [
       {
-        url: "/images/4dx/new/Stacked_4DX_Summit_Updated_LogoTransp.png",
+        url: "/images/4dx/26/3721x4300 pxl-01.jpeg",
         width: 1200,
         height: 630,
-        alt: "4DX CEO Summit 2024",
+        alt: `4DX CEO Summit ${new Date().getFullYear()}`,
       }
     ],
     type: 'website',
     locale: 'en_US',
-    siteName: '4DX CEO Summit 2024',
+    siteName: `4DX CEO Summit ${new Date().getFullYear()}`,
   },
   twitter: {
     card: "summary_large_image",
-    title: "4DX CEO Summit 2024",
+    title: `4DX CEO Summit ${new Date().getFullYear()}`,
     description: "Join us for insightful discussions, networking opportunities, and strategic collaborations shaping the future of technology and innovation across Africa",
-    images: [
-      "https://4dxceosummit.com/images/4dx/new/Stacked_4DX_Summit_Updated_LogoTransp.png",
-      "https://4dxceosummit.com/images/4dx/new/Stacked w date_4DX Summit Updated LogoTransp.png",
-      "https://4dxceosummit.com/images/4dx/new/Horiz_4DX Summit updated LogoTransp.png"
+    // images: [
+    //   "https://4dxceosummit.com/images/4dx/new/Stacked_4DX_Summit_Updated_LogoTransp.png",
+    //   "https://4dxceosummit.com/images/4dx/new/Stacked w date_4DX Summit Updated LogoTransp.png",
+    //   "https://4dxceosummit.com/images/4dx/new/Horiz_4DX Summit updated LogoTransp.png"
 
-    ],
+    // ],
   },
 };
 
